5de7f39f9de395d27ece3b01c3daaa295a63470e
Hr7iod <hr7iod@gmail.com>
2020-10-12 23:45:58 +0400

    Editing posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,3 +1,4 @@
+import { call } from "react-native-reanimated";
 import createDataContext from "./createDateContext";
 
 const blogReducer = (state, action) => {
@@ -13,6 +14,10 @@ const blogReducer = (state, action) => {
           content: action.payload.content,
         },
       ];
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     default:
       return state;
   }
@@ -21,7 +26,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
 
@@ -31,8 +38,20 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: "edit_blogpost",
+      payload: { id, title, content },
+    });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }]
 );
